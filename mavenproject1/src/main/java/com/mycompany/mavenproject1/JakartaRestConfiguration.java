688d3879f5b70e23422e678c4b4dc4a1a108ef74
Jared Brannen <jaredtylerbrannen678@gmail.com>
2024-09-27 18:49:58 -0500

    Added a Hello World page!

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.mavenproject1;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
